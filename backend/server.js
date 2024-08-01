2af0b3bad734158618537203100962e5eb0838cc
Zeyad Lotfy <zezoramy380@gmail.com>
2024-08-01 09:54:30 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 import authRouter from './routes/auth.routes.js'
 import messageRouter from './routes/message.routes.js'
 import userRouter from './routes/user.routes.js'
@@ -12,6 +12,8 @@ import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
@@ -20,6 +22,11 @@ app.use('/api/auth', authRouter)
 app.use('/api/messages', messageRouter)
 app.use('/api/users', userRouter)
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend" , "dist" , "index.html'))
+})
 server.listen(PORT, () => {
     connectToDB()
     console.log(`server running on ${PORT}`)
